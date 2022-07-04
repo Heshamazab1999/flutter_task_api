91c37c0e948bdda7cb6178a3028b5a92fbcc7d4f
Heshamazab1999 <60970771+Heshamazab1999@users.noreply.github.com>
2022-07-04 06:43:28 -0700

    Initial commit

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,23 +1,33 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_task/helper/app_constants.dart';
 import 'package:flutter_task/helper/images.dart';
 import 'package:flutter_task/screens/login_screen.dart';
-import 'package:splashscreen/splashscreen.dart';
 
-class Splash extends StatelessWidget {
+class Splash extends StatefulWidget {
   const Splash({Key? key}) : super(key: key);
+
+  @override
+  State<Splash> createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        const Duration(seconds: 3),
+        () => Navigator.pushReplacement(context,
+            MaterialPageRoute(builder: (context) => const LoginScreen())));
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SplashScreen(
-          useLoader: false,
-          seconds: 2,
-          navigateAfterSeconds: const LoginScreen(),
-          photoSize: 80,
-          image: Image.asset(
-            Images.logo,
-          )),
+      body: Center(
+        child: Image.asset(Images.logo,width: 150,height: 150),
+      ),
       backgroundColor: AppConstants.whiteColor,
     );
   }
